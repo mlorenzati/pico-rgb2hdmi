fc7ddf6ee6a7fbbd5dc02f557e2361384a73a72b
Marcelo Lorenzati <mlorenzati@gmail.com>
2022-08-09 23:37:13 -0300

    tool:convert hexa csv into png 8 and 16 bits

--- tools/csv2png.py
+++ tools/csv2png.py
@@ -9,29 +9,46 @@ if len(sys.argv)!=2:
     print('Usage: ', sys.argv[0], 'file.csv')
     exit(1)
 
-# Read 16-bit RGB565 image into array of uint16
+# Read 16-bit rgbPixel / 8bit RGb332 image into array of uint
+filame = sys.argv[1]
 try:
-    with open(sys.argv[1],'r') as file:
-        rgb565array = np.genfromtxt(file, delimiter = ',').astype(np.uint16)
+    with open(filame,'r') as file:
+        firstArray = (file.readline()).split(',')
+        width = len(firstArray)
+        wordSize = len(firstArray[0]) * 4
+        rgbArray = np.loadtxt(file, delimiter = ',', dtype='int16', converters = {_:lambda s: int(s, 16) for _ in range(width)})
 except Exception as e: 
     print('file error ', e)
     exit(1)
+
 # Pick up image dimensions
-h, w = rgb565array.shape
+h, w = rgbArray.shape
+
+# Report format
+print(filame, "is a ", w, "*", h+1, " with ", wordSize, " bits color density")
 
 # Make a numpy array of matching shape, but allowing for 8-bit/channel for R, G and B
 rgb888array = np.zeros([h,w,3], dtype=np.uint8)
 
 for row in range(h):
     for col in range(w):
-        # Pick up rgb565 value and split into rgb888
-        rgb565 = rgb565array[row,col]
-        r = ((rgb565 >> 11 ) & 0x1f ) << 3
-        g = ((rgb565 >> 5  ) & 0x3f ) << 2
-        b = ((rgb565       ) & 0x1f ) << 3
+        # Pick up rgbPixel value and split into rgb888
+        rgbPixel = rgbArray[row,col]
+        if  (wordSize == 16):
+            r = ((rgbPixel >> 11 ) & 0x1f ) << 3
+            g = ((rgbPixel >> 5  ) & 0x3f ) << 2
+            b = ((rgbPixel       ) & 0x1f ) << 3
+        elif (wordSize == 8):
+            r = ((rgbPixel >> 5  ) & 0x7 ) << 5
+            g = ((rgbPixel >> 2  ) & 0x7 ) << 5
+            b = ((rgbPixel       ) & 0x3 ) << 6
+        else:
+            print('file formatting error, neither 8 or 16 bits hexa, comma separated')
+            exit(1) 
         # Populate result array
         rgb888array[row,col]=r,g,b
 
 # Save result as PNG
 outputFile=sys.argv[1].rsplit('.')[0] + '.png'
-Image.fromarray(rgb888array).save(outputFile)
+aspectRatio43Height=int((w*3)/4)
+Image.fromarray(rgb888array).resize((w, aspectRatio43Height), resample=Image.NEAREST).save(outputFile)
